de19afbf029ef2a8ef797668e8d7d9f2849754c7
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 15:07:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,12 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map { |row| Student.new_from_db(row) }
   end
 
   def self.all_students_in_grade_9
