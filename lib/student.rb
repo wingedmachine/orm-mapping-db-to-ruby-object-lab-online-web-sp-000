9d79e5d8357cff3be45b0a57b9cd5a7399e2d534
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-02 15:06:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = '9th'
+      WHERE grade = 9
     SQL
 
     DB[:conn].execute(sql).map { |row| Student.new_from_db(row) }
